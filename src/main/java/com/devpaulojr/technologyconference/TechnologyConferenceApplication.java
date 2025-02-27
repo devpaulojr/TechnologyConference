72a1a9ed0f6db18cad60afd5ac49cf9c23fc8b61
devpaulojr <paulosantanajunior133@gmail.com>
2025-02-27 09:33:59 -0300

    configuração inicial do projeto

---
+++ src/main/java/com/devpaulojr/technologyconference/TechnologyConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.devpaulojr.technologyconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyConferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpaulojr/technologyconference/TechnologyConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpaulojr.technologyconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
