7672bdf25405f8de7e94c51186b23763249a2a73
devpaulojr <paulosantanajunior133@gmail.com>
2025-03-18 09:50:52 -0300

    feat: CompanyDto + CompanyMapper

--- src/main/java/com/devpaulojr/technologyconference/controllers/dtos/CompanyDto.java
+++ src/main/java/com/devpaulojr/technologyconference/controllers/dtos/CompanyDto.java
@@ -1,4 +1,47 @@
 package com.devpaulojr.technologyconference.controllers.dtos;
 
-public class CompanyDto {
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record CompanyDto(
+        UUID id,
+
+        @NotBlank(message = "Nome não pode ser vazio.")
+        @Size(message = "Nome inválido, tente outra nome", min = 1, max = 80)
+        String name,
+
+        @NotBlank(message = "CNPJ não pode ser vazio.")
+        @CNPJ(message = "CNPJ inválido.")
+        String cnpj,
+
+        @NotBlank(message = "Endereço não pode ser vazio.")
+        @Size(message = "Endereço inválido, tente outro endereço", min = 1, max = 80)
+        String address,
+
+        @NotBlank(message = "Rua não pode ser vazio.")
+        @Size(message = "Bairro inválido, tente outra bairro", min = 1, max = 80)
+        String neighborhood,
+
+        @NotBlank(message = "Cidade não pode ser vazio.")
+        @Size(message = "Cidade inválido, tente outra cidade", min = 1, max = 80)
+        String city,
+
+        @NotBlank(message = "Estado não pode ser vazio.")
+        @Size(message = "Estado inválido, digite apenas dois caracteres.", min = 2, max = 2)
+        String state,
+
+        @PastOrPresent(message = "data não pode ser superior o dia de hoje.")
+        LocalDateTime createdAt,
+
+        @FutureOrPresent(message = "data não pode ser inferior ao dia de hoje.")
+        LocalDateTime updatedAt,
+
+        List<UserCreatedDto> users) {
 }
--- src/main/java/com/devpaulojr/technologyconference/controllers/mappers/CompanyMapper.java
+++ src/main/java/com/devpaulojr/technologyconference/controllers/mappers/CompanyMapper.java
@@ -1,4 +1,17 @@
 package com.devpaulojr.technologyconference.controllers.mappers;
 
+import com.devpaulojr.technologyconference.controllers.dtos.CompanyDto;
+import com.devpaulojr.technologyconference.model.Company;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface CompanyMapper {
+
+    @Mapping(source = "users", target = "users")
+    CompanyDto toDto(Company company);
+
+    @Mapping(source = "users", target = "users")
+    Company toEntity(CompanyDto dto);
+
 }
