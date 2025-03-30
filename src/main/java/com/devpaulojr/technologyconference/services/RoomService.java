594769272eacda22879e7d5b812a6d6b619cb125
devpaulojr <paulosantanajunior133@gmail.com>
2025-03-30 12:55:22 -0300

    refactor: Atualização no método de salvar a entidade - RoomService

--- src/main/java/com/devpaulojr/technologyconference/services/RoomService.java
+++ src/main/java/com/devpaulojr/technologyconference/services/RoomService.java
@@ -1,11 +1,14 @@
 package com.devpaulojr.technologyconference.services;
 
+import com.devpaulojr.technologyconference.controllers.exceptions.ResourceNotFoundException;
 import com.devpaulojr.technologyconference.model.Room;
-import com.devpaulojr.technologyconference.model.User;
+import com.devpaulojr.technologyconference.model.enums.RoomStatus;
 import com.devpaulojr.technologyconference.repositories.CompanyRepository;
 import com.devpaulojr.technologyconference.repositories.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.devpaulojr.technologyconference.services.validations.RoomValidation.*;
 
 import java.util.List;
 
@@ -21,14 +24,22 @@ public class RoomService {
         return roomRepository.findAll();
     }
 
-    public Room insert(Room room){
+    public Room insert(Room room) {
 
-        var company = companyRepository.findById(room.getCompany().getId());
+        if (room.getCompany() != null && room.getCompany().getId() != null) {
+            var company = companyRepository.findById(room.getCompany().getId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Não foi possível achar o id: " + room.getCompany().getId()));
 
-        if(company.isPresent()){
-            room.setCompany(company.get());
-            room.getCompany().setUsers(company.get().getUsers());
+            room.setCompany(company);
+
+            //Validação da classe "Room"
+            validate(room);
+        }
+
+        if (room.getCompany() != null && (room.getCompany()).getUsers().isEmpty()) {
+            room.setRoomStatus(RoomStatus.valueOf("RESERVADO"));
         }
         return roomRepository.save(room);
     }
+
 }
