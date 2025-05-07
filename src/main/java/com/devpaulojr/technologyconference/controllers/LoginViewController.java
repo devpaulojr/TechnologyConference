d7bfa75a02d08dfc07215c9582eb272947c08c42
devpaulojr <paulosantanajunior133@gmail.com>
2025-05-07 20:04:31 -0300

    feat: Implementação básica do springSecurity

--- src/main/java/com/devpaulojr/technologyconference/controllers/LoginViewController.java
+++ src/main/java/com/devpaulojr/technologyconference/controllers/LoginViewController.java
@@ -1,4 +1,13 @@
 package com.devpaulojr.technologyconference.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginViewController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
 }
