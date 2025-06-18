e86384319d65696941e1df1e906de4c0c0611bf7
devpaulojr <paulosantanajunior133@gmail.com>
2025-06-18 08:57:35 -0300

    Fix: Correção de bug na classe - SecurityConfiguration

--- src/main/java/com/devpaulojr/technologyconference/configuration/SecurityConfiguration.java
+++ src/main/java/com/devpaulojr/technologyconference/configuration/SecurityConfiguration.java
@@ -1,11 +1,18 @@
 package com.devpaulojr.technologyconference.configuration;
 
+import com.devpaulojr.technologyconference.security.service.AuthenticationService;
+import com.devpaulojr.technologyconference.security.service.UserAccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,15 +21,43 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(login ->
+
+                .formLogin( login ->
                         login.loginPage("/login").permitAll())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/img/**", "/login").permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
+
+                .authorizeHttpRequests(authorize -> {
+
+                    authorize.requestMatchers("/login/**").permitAll();
+
+                    authorize.requestMatchers("/img/**").permitAll();
+
+                    authorize.requestMatchers(HttpMethod.POST, "/userAccounts/**").permitAll();
+
+                    authorize.anyRequest().authenticated();
+
+                })
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountService service){
+        return new AuthenticationService(service);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
